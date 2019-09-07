8d9755640c9b1d0f100cabc3a671c99d6ead76be
rosemaryfox <rosemaryfox@10.0.1.73>
2019-09-07 14:17:22 -0400

    Fixed resource writer & updated w/ 2019 names

--- src/main/java/dev/fox/data/SaveOutput.java
+++ src/main/java/dev/fox/data/SaveOutput.java
@@ -3,6 +3,8 @@ package dev.fox.data;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import com.opencsv.CSVReader;
@@ -35,7 +37,9 @@ public class SaveOutput {
 	}
 
 	public void save(List<String[]> records) throws IOException {
-		CSVReader reader = new CSVReader(new FileReader(originalFile), ';', '"');
+		InputStreamReader isr = new InputStreamReader(getClass().getClassLoader()
+                .getResourceAsStream(originalFile));
+		CSVReader reader = new CSVReader(isr, ';', '"');
 		List<String[]> currentLines = reader.readAll();
 		CSVWriter writer = new CSVWriter(new FileWriter(outputFile), ';', '"');
 		writer.writeAll(currentLines);
